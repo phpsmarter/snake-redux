78e11f6ebcc0fd81658fc3e0711a69f87fc5e546
Tom Wilson <tom@jackhq.com>
2017-09-23 13:02:51 -0400

    added touch events

--- app.js
+++ app.js
@@ -1,28 +1,50 @@
 import React from 'react'
-
-const { Layer, Stage } = ReactKonva
+import ReactTouchEvents from 'react-touch-events'
+import { connect } from 'react-redux'
+import R from 'ramda'
 
 import Board from './components/board'
 import Snake from './components/snake'
 import Food from './components/food'
 import Score from './components/score'
 
+const { toUpper } = R
+const { Layer, Stage } = ReactKonva
+
 const App = props => {
   return (
-    <div className="">
-      <h1 className="avenir tc mb2 ttu tracked">Snake</h1>
-      <div className="flex items-center">
-        <Stage className="center" width={400} height={400}>
-          <Layer>
-            <Board />
-            <Snake />
-            <Food />
-          </Layer>
-        </Stage>
+    <ReactTouchEvents
+      onSwipe={direction => {
+        let d = 'RIGHT'
+        if (direction === 'bottom') {
+          d = 40 // DOWN
+        } else if (direction === 'top') {
+          d = 38 // UP
+        } else if (direction === 'left') {
+          d = 37 // LEFT
+        } else {
+          d = 39 // RIGHT
+        }
+        props.dispatch({ type: 'CHANGE', payload: d })
+      }}
+    >
+      <div className="">
+        <h1 className="avenir tc mb2 ttu tracked">Snake</h1>
+        <div className="flex items-center">
+          <Stage className="center" width={400} height={400}>
+            <Layer>
+              <Board />
+              <Snake />
+              <Food />
+            </Layer>
+          </Stage>
+        </div>
+        <Score />
       </div>
-      <Score />
-    </div>
+    </ReactTouchEvents>
   )
 }
 
-export default App
+const connector = connect(state => ({ app: state.app }))
+
+export default connector(App)
--- components/score.js
+++ components/score.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 const Score = props => {
-  return <div>Score: {props.score}</div>
+  return <div className="tc">Score: {props.score}</div>
 }
 
 const connector = connect(state => ({ score: state.score }))
